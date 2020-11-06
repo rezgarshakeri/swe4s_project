21080ad26bbcc618cf0f65a2b5727c64076924bd
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-11-05 22:10:13 -0700

    src/heat2d.py: style and comments

--- src/heat2d.py
+++ src/heat2d.py
@@ -1,20 +1,28 @@
-import numpy as np
-import variables as var
 import math
+import numpy as np
+
+import variables as var
 import FE_subroutines as FE
 
+
 def main():
+
+    # Forms the assembly matrix by looping over all elements
     for e in range(var.nel):
         FE.assembly(e)
 
+    # Adds the forcing term causing by heat flux
     FE.src_flux(var.neq, var.nbe, var.ngp)
 
+    # Solves the linear system
     d = FE.solvedr(var.neq, var.nd)
-        
+
+    # Prints out the Temperatures at each node
+    # TODO: We need to create plots instead of printing
     print(d)
+
     return
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     main()
-
-

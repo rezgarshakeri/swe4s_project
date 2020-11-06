ee5626c39daa7a255aef869d3299270c39557679
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2020-11-06 15:45:21 -0700

    heat2d: added argsparse

--- src/heat2d.py
+++ src/heat2d.py
@@ -1,27 +1,75 @@
 import math
 import numpy as np
-
-import variables as var
+import math
 import FE_subroutines as FE
+import argparse
+
 
 
 def main():
+    """
+    This code solves 2d steady state heat equation
+    using finite element numerical method.
+    The output is the nodal temperature
+    """
 
-    # Forms the assembly matrix by looping over all elements
-    for e in range(var.nel):
-        FE.assembly(e)
+    parser = argparse.ArgumentParser(description='Input for FE_subroutines')
 
-    # Adds the forcing term causing by heat flux
-    FE.src_flux(var.neq, var.nbe, var.ngp)
+    parser.add_argument('--num_elm_x',
+                        dest='num_elm_x',
+                        type=int,
+                        default=2,
+                        help='Number of element in x direction')
 
-    # Solves the linear system
-    d = FE.solvedr(var.neq, var.nd)
+    parser.add_argument('--num_elm_y',
+                        dest='num_elm_y',
+                        type=int,
+                        default=3,
+                        help='Number of element in y direction')
 
-    # Prints out the Temperatures at each node
-    # TODO: We need to create plots instead of printing
+    parser.add_argument('--T0_bottom',
+                        dest='T0_bottom',
+                        type=float,
+                        default=10,
+                        help='Prescribed temperature on the bottom edge')
+
+    parser.add_argument('--T0_left',
+                        dest='T0_left',
+                        type=float,
+                        default=-10,
+                        help='Prescribed temperature on the left edge')
+
+    parser.add_argument('--heat_source',
+                        dest='heat_source',
+                        type=float,
+                        default=6,
+                        help='Heat source on the domain')
+
+    parser.add_argument('--flux_top',
+                        dest='flux_top',
+                        type=float,
+                        default=0,
+                        help='Prescribed flux on the top edge')
+    args = parser.parse_args()
+
+    nelx = args.num_elm_x
+    nely = args.num_elm_y
+    T0_bottom = args.T0_bottom
+    T0_left = args.T0_left
+    s0 = args.heat_source
+    flux_top = args.flux_top
+    # Gauss point, 2 is better than 1, has less errors
+    ngp = 2
+    # assemble stiffness and forcing vector
+    K, f = FE.assembly(nelx, nely, ngp, s0, T0_bottom, T0_left)
+    # update forcing vector f by adding flux vector
+    F = FE.src_flux(nelx, nely, T0_bottom, T0_left, flux_top, ngp, f)
+    # get the prescribed temperature on the boundary nodes
+    d0, ID, LM = FE.setup_ID_LM(nelx, nely, T0_bottom, T0_left)
+    # find the nodal temperature on the domain
+    d = FE.solvedr(nelx, nely, K, F, d0)
+    # print the results
     print(d)
-
-    return
 
 
 if __name__ == '__main__':

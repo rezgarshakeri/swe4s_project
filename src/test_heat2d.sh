0763f153b2fd90ad6cf0a8744b6100226b24ccd2
Rezgar Shakeri <Rezgar.Shakeri@colorado.edu>
2020-11-10 20:04:44 -0700

    test_heat2d.sh: functional test added

---
+++ src/test_heat2d.sh
@@ -0,0 +1,19 @@
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run test_heat2d_1 python heat2d.py
+assert_exit_code 0
+assert_in_stdout  10.        
+assert_in_stdout  10.
+assert_in_stdout  10.
+assert_in_stdout  -10.
+assert_in_stdout  -10.
+assert_in_stdout  -10.
+
+run test_heat2d_2 python heat2d.py --num_elm_x 2 --num_elm_y 2 --T0_bottom 5 --T0_left -2 --heat_source 0 --flux_top 0
+assert_exit_code 0
+assert_in_stdout  5.        
+assert_in_stdout  5.
+assert_in_stdout  5.
+assert_in_stdout  -2.
+assert_in_stdout  -2.
